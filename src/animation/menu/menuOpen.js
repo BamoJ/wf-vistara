4990482c4ebb9b819196a47624091c21a7f2148a
BamoJ <94501520+BamoJ@users.noreply.github.com>
2024-04-28 15:17:12 +0700

    finsh menu animation

--- src/animation/menu/menuOpen.js
+++ src/animation/menu/menuOpen.js
@@ -4,6 +4,7 @@ export default class MenuOpen {
 	constructor() {
 		this.el = {
 			menu: document.querySelector('.info_menu_wrap'),
+			menuWrapper: document.querySelector('.info_menu_inner'),
 			menuSVG: document.querySelector('.info_svg'),
 			menuStars: document.querySelector('.info_stars'),
 			menuClose: document.querySelector('.info_close_text'),
@@ -12,6 +13,10 @@ export default class MenuOpen {
 			colTxt: document.querySelectorAll('.colophon_item_txt'),
 			menuButton: document.querySelector('.nav_link-item.info'),
 			menuBG: document.querySelector('.info_menu_bg'),
+			lineTopBtm: document.querySelectorAll(
+				'[data-anim="info-line-top"]',
+			),
+			lineCol: document.querySelectorAll('[data-anim="info-col-item"]'),
 		};
 
 		this.scroll = new SmoothScroll();
@@ -36,7 +41,7 @@ export default class MenuOpen {
 
 		this.openTl.to(this.el.menu, {
 			transform: 'translateX(0%)',
-			duration: 1.25,
+			duration: 1.1,
 			ease: 'expo.inOut',
 		});
 		this.openTl
@@ -49,25 +54,104 @@ export default class MenuOpen {
 				},
 				0,
 			)
+			.fromTo(
+				this.el.menuSVG,
+				{
+					yPercent: 125,
+				},
+				{
+					yPercent: 0,
+					duration: 1,
+					ease: 'power4.out',
+				},
+				'<+.6',
+			)
 			.from(
-				this.menuPL,
+				this.el.menuStars,
 				{
 					yPercent: 100,
 					duration: 1,
 					ease: 'power4.out',
-					stagger: 0.15,
 				},
-				'<+0.5',
+				'<',
+			)
+			.from(
+				this.el.menuClose,
+				{
+					yPercent: 100,
+					duration: 1,
+					ease: 'power4.out',
+				},
+				'<+.1',
+			)
+			.from(
+				this.el.lineTopBtm,
+				{
+					width: '0%',
+					duration: 1.5,
+					ease: 'power4.out',
+					stagger: 0.4,
+				},
+				'<',
+			)
+			.from(
+				this.menuPL,
+				{
+					yPercent: 100,
+					duration: 0.85,
+					ease: 'power4.out',
+					stagger: 0.1,
+				},
+				'<+0.1',
 			)
 			.from(
 				this.menuPs,
 				{
 					yPercent: 100,
-					duration: 1,
+					duration: 0.85,
+					ease: 'power4.out',
+					stagger: 0.1,
+				},
+				'<+0.3',
+			)
+			.from(
+				this.el.colTitle,
+				{
+					yPercent: 120,
+					duration: 0.85,
+					ease: 'power4.out',
+				},
+				'<-0.3',
+			)
+			.from(
+				this.el.lineCol,
+				{
+					width: '0%',
+					duration: 1.1,
 					ease: 'power4.out',
 					stagger: 0.15,
 				},
-				'<+0.2',
+				'<',
+			)
+			.from(
+				this.el.colTxt,
+				{
+					yPercent: 110,
+					duration: 0.85,
+					ease: 'power3.out',
+					stagger: 0.1,
+				},
+				'<+0.1',
+			)
+			.from(
+				'.info_btm_text',
+				{
+					yPercent: 100,
+					duration: 1,
+					ease: 'power4.out',
+					stagger: 0.2,
+				},
+				'<+.1',
 			);
 	}
 
@@ -75,6 +159,9 @@ export default class MenuOpen {
 		this.tl = gsap.timeline({
 			onComplete: () => {
 				this.scroll.startScroll();
+				gsap.set(this.el.menuWrapper, {
+					autoAlpha: 1,
+				});
 			},
 		});
 
@@ -83,11 +170,20 @@ export default class MenuOpen {
 		});
 
 		this.tl
-			.to(this.el.menu, {
-				transform: 'translateX(100%)',
-				duration: 1,
-				ease: 'expo.out',
+			.to(this.el.menuWrapper, {
+				autoAlpha: 0,
+				duration: 0.65,
+				ease: 'sine.out',
 			})
+			.to(
+				this.el.menu,
+				{
+					transform: 'translateX(100%)',
+					duration: 1,
+					ease: 'expo.out',
+				},
+				'<+0.15',
+			)
 			.to(
 				this.el.menuBG,
 				{
@@ -108,6 +204,7 @@ export default class MenuOpen {
 			this.menuClose();
 		});
 
+		// close menu when clicking on the background
 		this.el.menuBG.addEventListener('click', () => {
 			this.menuClose();
 		});
